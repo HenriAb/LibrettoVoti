822c98c0eaa9480e1ee565fc353af6f51f24e3a6
erlib <erlib@DESKTOP-SC3IJI0>
2021-03-08 11:20:19 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -24,4 +24,38 @@ public class Libretto {
 		}
 		return res;
 	}
+	/* NO GOOD
+	public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa da solo i voti
+		System.out.println();
+	}
+	
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sara poi il chiamante a stamparli
+	} */
+	
+	public List<Voto> listaVotiUguali(int punteggio){ //espongo a esterno cosa sto usando 
+		//restituisce solo i voti uguali al criterio
+		
+		List<Voto> risultato = new ArrayList<>();
+		for(Voto v : this.voti) {
+			if(v.getVoto() == punteggio) {
+				risultato.add(v); //stesso oggetto punta a due liste diverse
+			}
+		}
+		return risultato;
+	}
+	
+	//e' modo migliore TO DO
+	public Libretto votiUguali(int punteggio) { // ti do un libretto in cui ho filtrato i dati che mi servono
+	
+		Libretto risultato = new Libretto();
+		for(Voto v : this.voti) {
+			if(v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
 }
